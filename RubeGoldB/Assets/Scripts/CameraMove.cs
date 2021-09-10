704a7c4fb220cc8a4767ff2f0ae796f4e0a4661a
MihirKM <mihir.m@digipen.edu>
2021-09-10 16:10:37 -0700

    Begun to rework project.

---
+++ RubeGoldB/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,24 @@
+/**************************
+Camera movement script
+Made by Mihir
+9/10/2021
+**************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
